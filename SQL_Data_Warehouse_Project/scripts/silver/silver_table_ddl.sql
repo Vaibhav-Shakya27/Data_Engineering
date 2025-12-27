a91cc1b4970faa53f4661774889b04119e306ba2
Vaibhav-Shakya27 <152723502+Vaibhav-Shakya27@users.noreply.github.com>
2025-12-27 16:23:01 +0530

    Create silver_table_ddl.sql

---
+++ SQL_Data_Warehouse_Project/scripts/silver/silver_table_ddl.sql
@@ -0,0 +1,118 @@
+/*
+
+Objective: 
+The script creates all the underlying tables for the Database, under schema
+silver for datasets erp and crm
+
+The following tables are created:
+
+erp: 
+
+	silver.erp_cust_az12
+	silver.erp_loc_a101
+	silver.erp_px_cat_g1v2
+
+crm:
+	silver.crm_cust_info
+	silver.crm_prd_info
+	silver.crm_sales_details
+*/
+
+-- Creating ERP tables
+
+if object_id ('silver.erp_cust_az12','U') is not null
+	drop table silver.erp_cust_az12;
+
+
+create table silver.erp_cust_az12 (
+
+cid	NVARCHAR(50),
+bdate DATE ,
+gen NVARCHAR(50),
+dwh_creation_date DATETIME2 DEFAULT GETDATE()
+
+);
+
+GO
+
+if object_id ('silver.erp_loc_a101','U') is not null
+	drop table silver.erp_loc_a101;
+
+CREATE TABLE silver.erp_loc_a101 (
+cid	NVARCHAR(50),
+cntry NVARCHAR(50),
+dwh_creation_date DATETIME2 DEFAULT GETDATE()
+
+);
+
+GO
+
+if object_id ('silver.erp_px_cat_g1v2','U') is not null
+	drop table silver.erp_px_cat_g1v2;
+
+CREATE TABLE silver.erp_px_cat_g1v2 (
+
+id	NVARCHAR(50),
+cat	NVARCHAR(50),
+subcat	NVARCHAR(50),
+maintainence NVARCHAR(50),
+dwh_creation_date DATETIME2 DEFAULT GETDATE()
+
+);
+
+GO
+
+-- Creating CRM tables
+
+if object_id ('silver.crm_cust_info','U') is not null
+	drop table silver.crm_cust_info;
+
+CREATE TABLE silver.crm_cust_info (
+	cst_id INT,
+	cst_key NVARCHAR(50),
+	cst_firstname NVARCHAR(50),
+	cst_lastname NVARCHAR(50),
+	cst_marital_status CHAR,
+	cst_gndr CHAR,
+	cst_create_date DATE,
+	dwh_creation_date DATETIME2 DEFAULT GETDATE()
+
+
+);
+GO
+
+
+if object_id ('silver.crm_prd_info','U') is not null
+	drop table silver.crm_prd_info;
+
+CREATE TABLE silver.crm_prd_info (
+prd_id INT,
+prd_key	NVARCHAR(50),
+prd_nm	NVARCHAR(50),
+prd_cost INT,
+prd_line NVARCHAR(50),
+prd_start_dt DATETIME,
+prd_end_dt DATETIME,
+dwh_creation_date DATETIME2 DEFAULT GETDATE()
+
+);
+GO
+
+
+if object_id ('silver.crm_sales_details','U') is not null
+	drop table silver.crm_sales_details;
+
+CREATE TABLE silver.crm_sales_details (
+sls_ord_num	NVARCHAR(50),
+sls_prd_key	NVARCHAR(50),
+sls_cust_id	INT,
+sls_order_dt INT,
+sls_ship_dt	INT,
+sls_due_dt	INT,
+sls_sales INT,
+sls_quantity INT,
+sls_price INT,
+dwh_creation_date DATETIME2 DEFAULT GETDATE()
+
+);
+GO
